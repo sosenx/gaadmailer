5dbe3db9f9598733be672e4e1d990166d46afbdd
sosenx <barteksosnowski711@gmail.com>
2017-11-07 02:39:25 +0100

    test 5

--- theme_files/page-gaad-imap-watch.php
+++ theme_files/page-gaad-imap-watch.php
@@ -21,7 +21,7 @@ if ( isset( $_GET[ IMAP_GET_VAR_NAME ] ) ) {
         'workers' => array( 
           'wyslij-email-z-platnosciami' => '/imap-workers/fotokalendarzeZamowienieEmailAction.php',
           'wyslij-email-do-gada' => '/imap-workers/testEmailAction.php',
-          'test2-email-action' => '/imap-workers/test2EmailAction.php'
+          'test2-email-action' => '/imap-workers/test2EmailAction.php' 
         )
       );
       $imap_worker = new imapwatch\imapWorker( $imap_worker_config );
